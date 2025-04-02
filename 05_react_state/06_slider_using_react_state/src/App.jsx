c11feeffb570fd026743f04951358d08f5291878
NSaha27 <sahaniladri27y@gmail.com>
2025-04-03 00:03:06 +0530

    picture slider project using react state

--- 05_react_state/06_slider_using_react_state/src/App.jsx
+++ 05_react_state/06_slider_using_react_state/src/App.jsx
@@ -1,12 +1,42 @@
-
+import Slider from "./components/Slider";
 
 function App() {
-
+  const picture = [
+    {
+      title: "Bear",
+      image: "bear.jpg",
+    },
+    {
+      title: "Deer",
+      image: "deer.jpg",
+    },
+    {
+      title: "Elephant",
+      image: "elephant.jpg",
+    },
+    {
+      title: "Lion",
+      image: "lion.jpg",
+    },
+    {
+      title: "Sheep and Lamb",
+      image: "sheep_and_lamb.jpg",
+    },
+    {
+      title: "Tiger",
+      image: "tiger.jpg",
+    },
+    {
+      title: "Zebra",
+      image: "zebra.jpg",
+    },
+  ];
   return (
     <>
-      
+      <h1 className="title">Slider Project</h1>
+      <Slider pictureObj={picture} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 05_react_state/06_slider_using_react_state/src/components/Slider.jsx
+++ 05_react_state/06_slider_using_react_state/src/components/Slider.jsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
+import "./Style.css";
+
 function Slider({ pictureObj }) {
+  const [picNumber, setPicNumber] = useState(0);
+
+  function changeToPrev() {
+    if (picNumber > 0) setPicNumber(picNumber - 1);
+  }
+  function changeToNext() {
+    if (picNumber < pictureObj.length - 1) setPicNumber(picNumber + 1);
+  }
+
   return (
     <div className="slider">
-      <div className="picture-handler">
-        <div className="picture"></div>
-        <div className="slide-indicator">
-          {
-            pictureObj.map(pic => {
-              return 
-            })
-          }
-        </div>
+      <div
+        className="picture-handler"
+        style={{
+          backgroundImage: `url('images/${pictureObj[picNumber].image}')`,
+        }}
+      >
+        <ul className="slide-indicator">
+          {pictureObj.map((pic, indx) => {
+            return (
+              <li
+                key={indx}
+                className={`indicator ${indx === picNumber ? "active" : ""}`}
+              ></li>
+            );
+          })}
+        </ul>
       </div>
-      <div className="slide-handler-btns"></div>
+      <div className="slide-handler-btns">
+        <button className="btn" onClick={changeToPrev}>
+          Prev
+        </button>
+        <button className="btn" onClick={changeToNext}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
