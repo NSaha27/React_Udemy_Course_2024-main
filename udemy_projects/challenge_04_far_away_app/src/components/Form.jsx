92954a41c643ed959d2180a7ec74f05d9037ee65
NSaha27 <152137415+NSaha27@users.noreply.github.com>
2025-05-01 00:58:54 +0530

    Update Form.jsx

--- udemy_projects/challenge_04_far_away_app/src/components/Form.jsx
+++ udemy_projects/challenge_04_far_away_app/src/components/Form.jsx
@@ -29,23 +29,9 @@ function Form() {
             value={inputs.numOfItems || "0"}
             onChange={handleChange}
           >
-            <option value="1" selected>
-              1
-            </option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
+            {
+              Array.from({length: 20}, (_, i) => i + 1).map((num) => {<option value={num}>{num}</option>})
+            }
           </select>
           <input
             type="text"
