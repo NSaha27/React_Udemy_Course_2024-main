c592b80e4f705b7ac34c5f6dca08f367464b2cc5
Niladri Saha <152137415+NSaha27@users.noreply.github.com>
2025-05-03 12:06:49 +0530

    Delete components directory

--- components/MyButton02.jsx
+++
@@ -1,9 +0,0 @@
-function MyButton02({ count, onClick }) {
-  return (
-    <button className="btn btn-success" onClick={onClick}>
-      Clicked me {count} times!
-    </button>
-  );
-}
-
-export default MyButton02;
