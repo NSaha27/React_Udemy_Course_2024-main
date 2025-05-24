8358a84528bc6c7a00c7396ce8547455b800ae67
NSaha27 <sahaniladri27y@gmail.com>
2025-05-24 21:23:23 +0530

    event handling and handler functions

--- 08_children_props/examples/ex_02_handling_events/src/components/HealthyFoods.jsx
+++ 08_children_props/examples/ex_02_handling_events/src/components/HealthyFoods.jsx
@@ -1,11 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import style from "./css/HealthyFoods.module.css";
 
 import FoodItem from "./FoodItem";
 
 function HealthyFoods({ foodList }) {
   return (
-    <div className={`w-50 mx-auto`}>
+    <div className={`w-100`}>
       <ul className="list-group">
         {foodList.map((food, indx) => {
           return <FoodItem foodItem={food} key={indx} />;
