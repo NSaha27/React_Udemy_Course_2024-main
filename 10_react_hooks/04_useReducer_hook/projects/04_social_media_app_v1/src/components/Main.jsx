f4dd17dbb67a64f6a4d00058483f257a63d56745
NSaha27 <sahaniladri27y@gmail.com>
2025-07-29 00:59:50 +0530

    social media app v1 - some modification in Main.jsx component

--- 10_react_hooks/04_useReducer_hook/projects/04_social_media_app_v1/src/components/Main.jsx
+++ 10_react_hooks/04_useReducer_hook/projects/04_social_media_app_v1/src/components/Main.jsx
@@ -4,18 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import LoginStatusContext from "../store/login-status-context";
 import { UserContext } from "../store/user-context";
 
-import SignupForm from "./SignupForm";
 import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 
+import DisplayPost from "./DisplayPosts";
 import Sidebar from "./Sidebar";
 import Sponsor from "./Sponsor";
-import DisplayPost from "./DisplayPosts";
 
 function Main({ loginSignupStatus }) {
   const { loginStatus } = useContext(LoginStatusContext);
   const isLoggedIn = loginStatus;
   const { users } = useContext(UserContext);
-
+  
   return (
     <main className="row position-relative">
       <Sidebar />
